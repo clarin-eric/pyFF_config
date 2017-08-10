ff5eb36f845f1c59735f330e33c24a4641939951
André Moreira <andre@clarin.eu>
2017-08-10 11:51:11 +0200

    Initial commit for Docker branch

--- control.sh
+++ control.sh
@@ -1,9 +1,9 @@
 #!/bin/sh -x
 
-venv_path='venv/' #'/srv/Python/venvs/2014-11-20_SPF/'
-pyff_config_directory_path="${venv_path:?}/etc/pyff_config/"
+venv_path='/srv/Python/venvs/SPF/'
+pyff_config_directory_path="${venv_path:?}/etc/pyFF_config/"
+use_java_home='/usr/lib/jvm/java-1.8-openjdk/jre/'
 
-#cd "$(dirname $(readlink -f "${0}"))" &&
 
 _curl() {
     'curl' --verbose --silent --fail --show-error --ipv4 --tlsv1 \
@@ -50,7 +50,6 @@ pyff_fetch_md() {
     error="pyff_fetch_md: eduGAIN -> exit status: $?; $error"
 
     rsync --remove-source-files -auv "${temp_dir_path}/" "${id_feds_target_dir_path}"
-    # && mv -v -b -S 'orig' -T  "${id_feds_target_dir_path}" || error="pyff_fetch_md: error during swapping '${temp_dir_path}' and '${id_feds_target_dir_path}' -> exit status: $?; $error"
 
     if [ -n "${error}" ]; then
         printf '%s \n' 'One or more errors occurred: ' "$error" ;
@@ -68,7 +67,6 @@ pyff_activate() {
 }
 
 pyff_run() {
-    #cd "${pyff_config_directory_path}" &&
     printf '%s\n' "Running PyFF job ${1}"
     pyff --loglevel=INFO "${1}".fd
     return $?
@@ -77,7 +75,7 @@ pyff_run() {
 pyff_sign() {
     xmlsectool_parameters="--sign --digest SHA-512 --key /root/keys/SPF_signing_priv.pem --certificate /root/keys/SPF_signing_pub.crt --referenceIdAttributeName ID "
     old_JAVA_HOME="${JAVA_HOME}"
-    JAVA_HOME='/usr/lib/jvm/java-7-openjdk-amd64/jre/'
+    JAVA_HOME="${use_java_home}"
     export JAVA_HOME
     # TODO: use $output_dir_path
     # shellcheck disable=SC2086
@@ -85,17 +83,9 @@ pyff_sign() {
       'output/md_about_spf_sps.xml' $xmlsectool_parameters ||
     error="pyff_sign: md_about_spf_sps -> exit status: $?; $error"
     # shellcheck disable=SC2086
-    'xmlsectool.sh' --inFile 'output/prod_md_about_clarin_erics_idp.xml' --outFile \
-      'output/prod_md_about_clarin_erics_idp.xml' $xmlsectool_parameters ||
-    error="pyff_sign: prod_md_about_clarin_erics_idp -> exit status: $?; $error"
-    # shellcheck disable=SC2086
     'xmlsectool.sh' --inFile 'output/prod_md_about_clarin_erics_idm.xml' --outFile \
       'output/prod_md_about_clarin_erics_idm.xml' $xmlsectool_parameters ||
     error="pyff_sign: prod_md_about_clarin_erics_idm -> exit status: $?; $error"
-    # shellcheck disable=SC2086
-    'xmlsectool.sh' --inFile 'output/beta_md_about_clarin_erics_idm.xml' --outFile \
-      'output/beta_md_about_clarin_erics_idm.xml' $xmlsectool_parameters ||
-    error="pyff_sign: beta_md_about_clarin_erics_idm -> exit status: $?; $error"
     # shellcheck disable=SC2086
     'xmlsectool.sh' --inFile 'output/prod_md_about_spf_idps.xml' --outFile \
       'output/prod_md_about_spf_idps.xml' $xmlsectool_parameters ||
@@ -118,20 +108,14 @@ pyff_sign() {
 pyff_verify_signatures() {
     xmlsectool_parameters=" --verifySignature --certificate /root/keys/SPF_signing_pub.crt "
     old_JAVA_HOME="${JAVA_HOME}"
-    JAVA_HOME='/usr/lib/jvm/java-7-openjdk-amd64/jre/'
+    JAVA_HOME="${use_java_home}"
     export JAVA_HOME
     # shellcheck disable=SC2086
     'xmlsectool.sh' --inFile 'output/md_about_spf_sps.xml' $xmlsectool_parameters ||
     error="pyff_verify_signatures: md_about_spf_sps -> exit status: $?; $error"
     # shellcheck disable=SC2086
-    'xmlsectool.sh' --inFile 'output/prod_md_about_clarin_erics_idp.xml' $xmlsectool_parameters ||
-    error="pyff_verify_signatures: prod_md_about_clarin_erics_idp -> exit status: $?; $error"
-    # shellcheck disable=SC2086
     'xmlsectool.sh' --inFile 'output/prod_md_about_clarin_erics_idm.xml' $xmlsectool_parameters ||
     error="pyff_verify_signatures: prod_md_about_clarin_erics_idm -> exit status: $?; $error"
-    # shellcheck disable=SC2086
-    'xmlsectool.sh' --inFile 'output/beta_md_about_clarin_erics_idm.xml' $xmlsectool_parameters ||
-    error="pyff_verify_signatures: beta_md_about_clarin_erics_idm -> exit status: $?; $error"
     # shellcheck disable=SC2086
     'xmlsectool.sh' --inFile 'output/prod_md_about_spf_idps.xml' $xmlsectool_parameters ||
     error="pyff_verify_signatures: prod_md_about_spf_idps -> exit status: $?; $error"
@@ -151,17 +135,17 @@ pyff_verify_signatures() {
 
 pyff_publish() {
     old_JAVA_HOME="${JAVA_HOME}"
-    JAVA_HOME='/usr/lib/jvm/java-7-openjdk-amd64/jre/'
+    JAVA_HOME="${use_java_home}"
     export JAVA_HOME
-    chown -Rv '0:www-data' 'output/' &&
+    chown -Rv '0:nginx' 'output/' &&
     chmod -Rv 'u=rw,g=r,o=' 'output/' &&
-    mv 'output/md_about_spf_sps.xml' 'output/prod_md_about_clarin_erics_idp.xml' \
-      'output/prod_md_about_clarin_erics_idm.xml' 'output/beta_md_about_clarin_erics_idm.xml' \
+    mv 'output/md_about_spf_sps.xml' \
+      'output/prod_md_about_clarin_erics_idm.xml' \
       'output/prod_md_about_spf_idps.xml' 'output/prod_md_about_spf_sps.xml' \
-      -t '/srv/www/infra.clarin.eu/aai/' &&
-    (cd '/srv/scripts/exec/SAML_metadata_QA_validator/' &&
+      '/srv/www/infra.clarin.eu/aai/' &&
+    (cd '/srv/installations/SAML_metadata_QA_validator/' &&
     ant &&
-    chown -v '0:www-data' 'out/md_about_spf_sps.svrlt' \
+    chown -v '0:nginx' 'out/md_about_spf_sps.svrlt' \
       'out/md_about_spf_sps_qa.xml' 'out/prod_md_about_spf_sps.svrlt' \
       'out/prod_md_about_spf_sps_qa.xml' &&
     chmod -v 'u=rw,g=r,o=' 'out/md_about_spf_sps.svrlt' \
@@ -169,7 +153,7 @@ pyff_publish() {
       'out/prod_md_about_spf_sps_qa.xml' &&
     cp -a 'out/md_about_spf_sps.svrlt' 'out/md_about_spf_sps_qa.xml' \
       'out/prod_md_about_spf_sps.svrlt' 'out/prod_md_about_spf_sps_qa.xml' \
-      -t '/srv/www/infra.clarin.eu/aai/') &&
+      '/srv/www/infra.clarin.eu/aai/') &&
     JAVA_HOME="${old_JAVA_HOME}"
     export JAVA_HOME &&
     return $?
